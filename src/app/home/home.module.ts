fc13b6d57cd265dc965e07e675fdeee352e32a98
AbinashBasak <abinashbasak129@gmail.com>
2023-03-13 23:03:49 +0530

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    QRCodeModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+  scanResult: any;
 
   constructor() {}
 
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+
+      if (status.granted) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async startScan() {
+    const hasPermission = await this.checkPermission();
+    if (!hasPermission) {
+      return;
+    }
+
+    try {
+      BarcodeScanner.hideBackground();
+      document.querySelector('body')?.classList.add('scanner-active');
+      const result = await BarcodeScanner.startScan();
+      if (result.hasContent) {
+        this.scanResult = result.content;
+      }
+    } catch (error) {}
+  }
+
+  stopScan() {
+    BarcodeScanner.showBackground();
+    document.querySelector('body')?.classList.remove('scanner-active');
+    BarcodeScanner.stopScan();
+  }
+
+  ngOnDestroy(): void {
+    this.startScan();
+  }
 }
